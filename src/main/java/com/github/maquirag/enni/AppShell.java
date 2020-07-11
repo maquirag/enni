83ab33986dfd66bf0edd7ef3ea808a8f994fca22
Tibor Santa <maquirag@gmail.com>
2020-07-11 16:57:48 +0200

    cleaned comment

--- src/main/java/com/github/maquirag/enni/AppShell.java
+++ src/main/java/com/github/maquirag/enni/AppShell.java
@@ -3,10 +3,6 @@ package com.github.maquirag.enni;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 
-/**
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- */
 @PWA(name = "ENhanced Nutrition Insights", shortName = "enni", enableInstallPrompt = false)
 public class AppShell implements AppShellConfigurator {
 }
